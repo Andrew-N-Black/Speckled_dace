c1a3d07e187e6d988bdd5f3ce5afeb5c0fcefdc3
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-08-10 11:38:45 -0700

    Update admixture.sh

--- admixture.sh
+++ admixture.sh
@@ -8,14 +8,14 @@ for i in 1 2 3 4 5 6 7 8; do ./admixture  --cv ./data/filtered.ped $i | tee log$
 #Check error rate for each model, select the lowest
 grep -h CV log?.out
 
-#CV error (K=1): 0.38356
-#CV error (K=2): 0.34932
-#CV error (K=3): 0.33937
-#CV error (K=4): 0.34333
-#CV error (K=5): 0.35044
-#CV error (K=6): 0.35498
-#CV error (K=7): 0.36601
-#CV error (K=8): 0.37958
+#CV error (K=1): 0.38616
+#CV error (K=2): 0.35144
+#CV error (K=3): 0.34230
+#CV error (K=4): 0.34769
+#CV error (K=5): 0.35219
+#CV error (K=6): 0.35952
+#CV error (K=7): 0.36828
+#CV error (K=8): 0.37709
 
 #Plot above using admixture.R
 #DONE
