f2857a3eb4427da0c46c1cfc9d925403fe486320
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-08-10 10:03:43 -0700

    Update div.R

--- div.R
+++ div.R
@@ -1,8 +1,8 @@
 #Diversity metrics calculated for five dace populations
 
 #programs used
-diveRsity v1.9.90
+#diveRsity v1.9.90
 library(diveRsity)
 #Run diversity calculation
-diver<-diffCalc(infile="/Users/andrew/dace_rerun/populations.snps.singlesnp.gen",outfile = "~/div",fst=TRUE,pairwise=TRUE,boots=1000,para=TRUE,bs_locus=TRUE,bs_pairwise=TRUE)
+diver<-diffCalc(infile="/Users/andrew/Library/CloudStorage/Box-Box/Foskett Dace Population Genetics/Working_Code/populations.snps.genepop",outfile = "~/div",fst=TRUE,pairwise=TRUE,boots=1000,para=TRUE,bs_locus=TRUE,bs_pairwise=TRUE)
 #DONE
