e416b7a241ac42cb801cc6cbe8163db0314fb889
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-08-10 15:50:56 -0700

    Update pca.R

--- pca.R
+++ pca.R
@@ -4,22 +4,22 @@
 library(adegenet)
 
 #Read plink file (recoded with plink, see plink.sh)
-dace<-read.PLINK("populations.plink.raw")
+dace<-read.PLINK("filtered.raw")
 #Run PCA
 pca1<-glPca(dace,nf=2,parallel = TRUE,n.cores = 4)
 #Plot
-s.class(pca1$scores, pop(dace),clabel = 0, cpoint = 1.5,col=c("black","black","black","grey","grey"),pch=shape[dace@pop])
+s.class(pca1$scores, pop(dace),clabel = 0, cpoint = 1.5,col=c("black","black","black","grey","grey"),pch=shape[dace@pop],grid=F)
 #Add eig
 b<-add.scatter.eig(pca1$eig,2,1,2, posi = "bottomright")
 
 #Percent variance for manual annotation to plot
 head(pca1$eig)
-[1] 14.966176  6.168516  3.001680  1.598602  1.557322  1.394309
+#[1] 15.531970  6.034579  3.162138  1.612524  1.478360  1.416109
 
-14.966176/sum(pca1$eig)*100
-[1] 9.15425
+15.531970/sum(pca1$eig)*100
+#9.392614
 
-6.168516/sum(pca1$eig)*100
-[1] 3.773051
+6.034579/sum(pca1$eig)*100
+#3.649278
 
-svg("Fig_3.svg")
+ggsave("Fig_3.svg")
