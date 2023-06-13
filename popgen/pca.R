587df7485c793fa50445d7e09051a2ea7f5e9e8e
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-06-13 09:06:27 -0700

    Update pca.R

--- popgen/pca.R
+++ popgen/pca.R
@@ -7,10 +7,15 @@ library(adegenet)
 dace<-read.PLINK("filtered.raw")
 #Run PCA
 pca1<-glPca(dace,nf=2,parallel = TRUE,n.cores = 4)
+
+#Bind
+s<-pca1$scores
+p<-as.data.frame(dace$pop)
+X<-cbind(s,p)
+
 #Plot
-s.class(pca1$scores, pop(dace),clabel = 0, cpoint = 1.5,col=c("black","black","black","grey","grey"),pch=shape[dace@pop],grid=F)
-#Add eig
-b<-add.scatter.eig(pca1$eig,2,1,2, posi = "bottomright")
+ggplot(data=X, aes(y=PC2, x=PC1))+geom_point(size=7,color="black",aes(shape=dace$pop,fill=dace$pop))+ theme_classic() + xlab("PC1 (9.4%)") +ylab("PC2 (3.6%)")+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+scale_fill_manual("Population", values=c("grey30","grey40","grey60","grey80","grey100"))+scale_shape_manual("Population", values=c(23,22,21,24,25))
+
 
 #Percent variance for manual annotation to plot
 head(pca1$eig)
